aa816c3a96e00f409f01b25734b8ac1e72e79a54
yus2276 <yus2276@CA-SU-F106-24.sduhsd.lan>
2020-02-13 10:34:10 -0800

    Finished Unit 5 Labs

--- APCS-A-2020/src/Unit5/VowelsRunner.java
+++ APCS-A-2020/src/Unit5/VowelsRunner.java
@@ -9,7 +9,7 @@ public class VowelsRunner
 	public static void main( String args[] )
 	{
 		FirstLastVowel s = new FirstLastVowel();
-			
+		
 		System.out.println(  FirstLastVowel.go(  "dog#cat#pigaplus")    );
 		System.out.println(  FirstLastVowel.go(  "pigs#apluscompsci#food")    );
 		System.out.println(  FirstLastVowel.go(  "##catgiraffeapluscompsci")    );
