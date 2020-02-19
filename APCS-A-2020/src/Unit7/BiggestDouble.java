f98c85f23d24e442145faa93b31d82144f702746
yus2276 <yus2276@CA-SU-F106-24.sduhsd.lan>
2020-02-19 11:11:48 -0800

    Started Unit 7

--- APCS-A-2020/src/Unit7/BiggestDouble.java
+++ APCS-A-2020/src/Unit7/BiggestDouble.java
@@ -16,19 +16,38 @@ public class BiggestDouble
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double temp = one;
+		if(two > temp) {
+			temp = two;
+		}
+		if(three > temp) {
+			temp = three;
+		}
+		if(four > temp) {
+			temp = four;
+		}
+		return temp;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return one + " " + two + " " + three + " " + four + " " + "\n biggest = " + getBiggest() ;
 	}
+
 }
--- APCS-A-2020/src/Unit7/DoubleRunner.java
+++ APCS-A-2020/src/Unit7/DoubleRunner.java
@@ -14,6 +14,19 @@ public class DoubleRunner
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
 		//add more test cases
-				
+		/*
+		 * 4.5 6.7 7.8 9.0
+		14.51 6.17 71.8 1.0
+		41.15 816.7 17.8 19.0
+		884.5 16.7 7.8 9.0
+		4.5 -456.7 677.8 9.0
+		4.5 16.7 -7.8 -9.0
+		 */
+		System.out.println(new BiggestDouble(14.51,6.17,71.8,1.0));
+		System.out.println(new BiggestDouble(14.51,6.17,71.8,1.0));
+		System.out.println(new BiggestDouble(14.51,6.17,71.8,1.0));
+		System.out.println(new BiggestDouble(14.51,6.17,71.8,1.0));
+		System.out.println(new BiggestDouble(14.51,6.17,71.8,1.0));
+
 	}
 }
