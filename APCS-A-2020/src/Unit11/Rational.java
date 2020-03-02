f464c6219f6e2cd8c9f6b5cd2dd8d1861bbf4049
yus2276 <yus2276@CA-SU-F106-24.sduhsd.lan>
2020-03-02 11:04:27 -0800

    Finished Unit 11 Rational

--- APCS-A-2020/src/Unit11/Rational.java
+++ APCS-A-2020/src/Unit11/Rational.java
@@ -8,30 +8,60 @@ import static java.lang.System.*;
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-
+	private int num1;
+	private int den1;
 	//write two constructors
 
-
-	//write a setRational method
+	public Rational() {
+		num1 = 1;
+		den1 = 1;
+	}
+	public Rational(int num, int den) {
+		num1 = num;
+		den1 = den;
+	}
 	
+	//write a setRational method
+	public void setRational(int num, int den) {
+		num1 = num; 
+		den1 = den;
+	}
 
 	//write  a set method for numerator and denominator
-
+	public void setNum(int num) {
+		num1 = num;
+	}
+	public void setDen(int den) {
+		den1 = den;
+	}
+	
 	
 	public void add(Rational  other)
 	{
-		//num1/den1 + num2/den2 
-		//new numerator = (num1 * den2 + num2 * den1)
-		//new denominator = (den1 * den2)
+		/*num1/den1 + num2/den2 
+		new numerator = (num1 * den2 + num2 * den1)
+		new denominator = (den1 * den2)
+		*/
+		int tempNum = den1 * other.getNum() ;
+		num1 *= other.getDen();
+		other.setNum(tempNum);
 		
+		den1 *= other.getDen();
 		
-
+		num1 += other.getNum();
+		
 
 		reduce();
 	}
 
 	private void reduce()
 	{
+		for(int i = num1; i > 0; i--) {
+			if(den1%i == 0 && num1%i == 0) {
+				num1 /=i;
+				den1 /= i;
+			}
+		}
 
 
 
@@ -40,39 +70,59 @@ class Rational implements Comparable<Rational>
 	private int gcd(int numOne, int numTwo)
 	{
 
+		int factor = 0;
+		for(int i = 0; i < numOne; i++) {
+			if(numOne%i == 0 && numTwo%i == 0) {
+				factor = i;
+			}
+		}
 
-		return 1;
+		return factor;
 	}
 
 	public Object clone ()
 	{
-		return "";
+		Rational clone = this;
+		return clone;
 	}
 
 
 	//ACCESSORS
 
 	//write get methods for numerator and denominator
-	
+	public int getNum() {
+		return num1;
+	}
+	public int getDen() {
+		return den1;
+	}
 	
 	public boolean equals( Object obj)
 	{
-
-
+		Rational other = (Rational)obj;
+		this.reduce();
+		other.reduce();
+		if(num1 == other.getNum() && den1 == other.getDen())
+			return true;
 		return false;
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
-		return -1;
+		if((double) num1/den1 > (double) other.getNum()/other.getDen())
+			return 1;
+		else if((double) num1/den1 < (double) other.getNum()/other.getDen())
+			return -1;
+		return 0;
 	}
 
 
 
 	
 	//write  toString() method
-	
+	public String toString() {
+		return num1 + "/" + den1;
+		
+	}
 	
 }
