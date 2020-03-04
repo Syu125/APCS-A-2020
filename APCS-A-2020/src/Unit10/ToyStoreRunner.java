b3993c8e2cb8d1f71bde3cd30270b57769b1b486
yus2276 <yus2276@CA-SU-F106-24.sduhsd.lan>
2020-03-04 11:12:58 -0800

    Almost done with Unit 10

--- APCS-A-2020/src/Unit10/ToyStoreRunner.java
+++ APCS-A-2020/src/Unit10/ToyStoreRunner.java
@@ -17,5 +17,7 @@ public class ToyStoreRunner
 	 * sorry bat sorry sorry sorry train train teddy teddy ball ball
 	 */
 		ArrayList <Toy> toys = new ArrayList <Toy>();
-		} 
+		Toy [] t = {new Toy("sorry"), new Toy( "bat"), new Toy("sorry"), new Toy("sorry"), new Toy(" sorry"), new Toy(" train"),  new Toy("train"), new Toy(" teddy"), new Toy(" teddy"), new Toy(" ball"), new Toy(" ball")};
+		
+	} 
 }
