b05bff8515421850b08c07c9bfc2fa2eb102eca7
yus2276 <yus2276@CA-SU-F106-24.sduhsd.lan>
2020-03-04 10:43:38 -0800

    Finished Graphics Unit 2

---
+++ APCS-A-2020/src/GraphicsUnit2/ShapePanel.java
@@ -0,0 +1,52 @@
+package GraphicsUnit2;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Font; 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ShapePanel extends JPanel
+{
+	public ShapePanel()
+	{
+		setBackground(Color.WHITE);
+		setVisible(true);
+	}
+
+	public void update(Graphics window)
+	{
+		paint(window);
+	}
+
+	/*
+	 *All of your test code should be placed in paint.
+	 */
+	public void paint(Graphics window)
+	{
+		window.setColor(Color.WHITE);
+		window.fillRect(0,0,getWidth(), getHeight());
+		window.setColor(Color.BLUE);
+		window.drawRect(20,20,getWidth()-40,getHeight()-40);
+		window.setFont(new Font("TAHOMA",Font.BOLD,18));
+		window.drawString("CREATE YOUR OWN SHAPE!",40,40);
+		Shape s = new Shape(100,100,100,100,Color.blue, 0,0);
+		Shape s2 = new Shape(200,150,300,100,Color.cyan, 0,0);
+
+		s.draw(window);
+		s2.draw(window);
+
+		//make a Shape
+		//draw the Shape
+	}
+}
